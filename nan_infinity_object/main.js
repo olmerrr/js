f8c8d67fb7b044d2508a647b2f7b293858da724e
Yaroslav <dombrovskiy91@gmail.com>
2019-07-25 21:59:38 +0300

    task from book
    task from book

---
+++ nan_infinity_object/main.js
@@ -0,0 +1,13 @@
+console.log( 4/0 );
+console.log( "string" * 6);
+
+let something;
+
+console.log( something );
+let persone = {
+    name: "Yaroslav",
+    age: 28,
+    isMarried: false
+};
+console.log( persone.name );
+console.log ( persone.age);
