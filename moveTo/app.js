e429505693f2d280c34f907d95cf7d06a966347f
Yaroslav <dombrovskiy91@gmail.com>
2022-03-18 16:01:29 +0200

    moveTo lib
    moveTo lib

--- moveTo/app.js
+++ moveTo/app.js
@@ -1,4 +1,5 @@
 const moveTo = new MoveTo();
+// беру все кнопки на странице
 const buttons = document.querySelectorAll('.button')
-
+// и вещаю на них trigger
 buttons.forEach(btn => moveTo.registerTrigger(btn));
